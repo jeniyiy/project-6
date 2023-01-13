0b220cf76cd029569af380ba31a2936bf02a8a9a
jenny <jennywidiastutiwd@gmail.com>
2023-01-13 11:20:50 +0700

    first jenny

---
+++ belajar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h3>Fundamental JS</h3>
+	<script>
+		var namaPanggilan = "Jenny";
+		var nama_panjang = "Jenny Widiastuti";
+
+		console.log(namaPanggilan);
+		console.log(nama_panjang);
+
+		namaBuah="Mangga";
+		namaSayur="Bayam";
+		namaHewan="Ayam";
+		namaHari="Senin";
+
+		document.write(namaBuah+ "<br/>" +namaSayur+ "<br/>" +namaHewan+ "<br/>" +namaHari);
+
+		// namaKendaraan="Mobil";
+		// namaOrang="Farisa";
+		// alert(namaOrang+ "naik" +namaKendaraan);
+
+
+		namaManusia="Riris";
+		namaMakanan="Nasi Goreng";
+		alert(namaManusia+ "makan" +namaMakanan);
+	</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h3>Belajar Operator Ternary</h3>
+	<script>
+		/*var namaPanggilan = "Jenny";
+		var nama_panjang = "Jenny Widiastuti";
+
+		console.log(namaPanggilan);
+		console.log(nama_panjang);
+
+		namaBuah="Mangga";
+		namaSayur="Bayam";
+		namaHewan="Ayam";
+		namaHari="Senin";
+
+		document.write(namaBuah+ "<br/>" +namaSayur+ "<br/>" +namaHewan+ "<br/>" +namaHari);
+
+		namaKendaraan="Mobil";
+		namaOrang="Farisa";
+		alert(namaOrang+ "naik" +namaKendaraan);
+
+		namaManusia="Riris ";
+		namaMakanan=" Nasi Goreng";
+		alert(namaManusia+ "makan" +namaMakanan);
+
+		var a="nama : Jenny";
+		var b="kelas : 12 RPL 2";
+		var c="Tempat / Tanggal Lahir : Jakarta 26 Januari 2005";
+		var d="Alamat : Bogor";
+
+		console.log(a);
+		console.log(b);
+		console.log(c);
+		console.log(d);
+
+		document.write(a +"<br/>" +b+ "<br/>" +c+ "<br/>" +d);
+
+		salam="Selamat Datang";
+		nama=" Jenny";
+		alert(salam+ nama);
+		
+		a = 20;
+		b = 45;
+		c = a+b;
+
+		document.write("a + b =" +c);
+
+
+		d = 100;
+		e = 50;
+		f = d-e;
+
+		document.write("<br/>" + "d - e =" +f); 
+
+
+
+		g = 5;
+		g+=1;
+		document.write(g);
+
+		h = 8;
+		h-=1;
+		document.write("<br/>" +h);*/
+
+		// var totalBelanja = prompt("total belanja", 0);
+		// var hasil = totalBelanja >=80?"Waw Boros" : "Waw Hemat";
+		// document.write("<br/>"+hasil);
+
+		var size = prompt("size", 0);
+		var hasill = size >=40?"Size di atas 40" : "Size di bawah 40";
+		document.write(hasill);
+	</script>
+
+</body>
+</html>
